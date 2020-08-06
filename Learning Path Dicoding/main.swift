e469337afa052d86cd6c7aa22501aa9fabc74319
danielprast <danielprastiwa@gmail.com>
2020-08-06 16:25:33 +0700

    inital Commit

---
+++ Learning Path Dicoding/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learning Path Dicoding
+//
+//  Created by Daniel Prastiwa on 06/08/20.
+//  Copyright © 2020 Danielprast. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
